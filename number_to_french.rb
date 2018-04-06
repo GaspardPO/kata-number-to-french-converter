d082a5e5302522f47ea2b86fe1399a007f6def17
GaspardPO <gaspard.pointeau@gmail.com>
2018-04-06 11:49:41 +0200

    add tens. extract zero as edge case.

--- number_to_french.rb
+++ number_to_french.rb
@@ -1,6 +1,11 @@
 require 'test/unit'
 
 def translate_to_french(number)
-  units_as_string = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
-  units_as_string[number]
+  if number == 0
+    return "zero"
+  end
+
+  units_as_string = ["", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
+  tens_as_string = ["", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingts", "quatre-vingt-dix"]
+  tens_as_string[number / 10] + units_as_string[number % 10]
 end
--- number_to_french_test.rb
+++ number_to_french_test.rb
@@ -2,16 +2,32 @@ require 'test/unit'
 require "./number_to_french"
 
 class NumberToFrenchTest < Test::Unit::TestCase
-  def test_return_units
-    assert_equal'zero',  translate_to_french(0)
-    assert_equal'un',  translate_to_french(1)
-    assert_equal'deux',  translate_to_french(2)
-    assert_equal'trois',  translate_to_french(3)
-    assert_equal'quatre',  translate_to_french(4)
-    assert_equal'cinq',  translate_to_french(5)
-    assert_equal'six',  translate_to_french(6)
-    assert_equal'sept',  translate_to_french(7)
-    assert_equal'huit',  translate_to_french(8)
-    assert_equal'neuf',  translate_to_french(9)
+  def test_return_zero
+    assert_equal 'zero', translate_to_french(0)
   end
+
+  def test_return_units
+    assert_equal 'un', translate_to_french(1)
+    assert_equal 'deux', translate_to_french(2)
+    assert_equal 'trois', translate_to_french(3)
+    assert_equal 'quatre', translate_to_french(4)
+    assert_equal 'cinq', translate_to_french(5)
+    assert_equal 'six', translate_to_french(6)
+    assert_equal 'sept', translate_to_french(7)
+    assert_equal 'huit', translate_to_french(8)
+    assert_equal 'neuf', translate_to_french(9)
+  end
+
+  def test_return_tens
+    assert_equal 'dix', translate_to_french(10)
+    assert_equal 'vingt', translate_to_french(20)
+    assert_equal 'trente', translate_to_french(30)
+    assert_equal 'quarante', translate_to_french(40)
+    assert_equal 'cinquante', translate_to_french(50)
+    assert_equal 'soixante', translate_to_french(60)
+    assert_equal 'soixante-dix', translate_to_french(70) # french style, not Belgian nor Suiss.
+    assert_equal 'quatre-vingts', translate_to_french(80)
+    assert_equal 'quatre-vingt-dix', translate_to_french(90)
+  end
+
 end
